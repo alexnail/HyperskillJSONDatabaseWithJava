6e0d8d0afcff24e9f6fba553f50194a15703153a
aleksandrsfridmans <alexnail@gmail.com>
2025-01-07 09:27:54 +0000

    Step 2. Step completed.

--- JSON Database with Java/task/src/client/Main.java
+++ JSON Database with Java/task/src/client/Main.java
@@ -1,7 +1,29 @@
 package client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Main {
+    private static final String ADDRESS = "127.0.0.1";
+    private static final int SERVER_PORT = 23456;
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        try (
+                Socket socket = new Socket(ADDRESS, SERVER_PORT);
+                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                ){
+            var record = 12;
+            System.out.println("Client started!");
+            var output = "Give me a record # %d".formatted(record);
+            dataOutputStream.writeUTF(output);
+            System.out.println("Sent: " + output);
+            var input = dataInputStream.readUTF();
+            System.out.println("Received: " + input);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- JSON Database with Java/task/src/server/Main.java
+++ JSON Database with Java/task/src/server/Main.java
@@ -1,14 +1,36 @@
 package server;
 
-import server.command.CommandPrompt;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
+    private static final String ADDRESS = "127.0.0.1";
+    private static final int SERVER_PORT = 23456;
+
     public static void main(String[] args) {
-
-        CommandPrompt commandPrompt = new CommandPrompt();
-
-        while (commandPrompt.command().execute()) {
-
+        System.out.println("Server started!");
+        try (
+                ServerSocket serverSocket = new ServerSocket(SERVER_PORT, 50, InetAddress.getByName(ADDRESS));
+                Socket socket = serverSocket.accept();
+                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+        ) {
+            var input = dataInputStream.readUTF();
+            System.out.println("Received: " + input);
+            var record = Integer.parseInt(input.substring("Give me a record # ".length()));
+            var output = "A record # %d was sent!".formatted(record);
+            dataOutputStream.writeUTF(output);
+            System.out.println("Sent: " + output);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
+        /*CommandPrompt commandPrompt = new CommandPrompt();
+        while (commandPrompt.command().execute()) {
+        }*/
     }
 }
